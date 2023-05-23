c866fe04d53f87eb41b14862ee3dc798a74a93b3
Hewerton <hewerton.fl@gmail.com>
2023-05-23 07:03:14 -0300

    Adição de funcionalidade: Streaming de imagem segmentada

--- app.py
+++ app.py
@@ -15,80 +15,111 @@ load_figure_template(["lux", "cyborg", "minty", "pulse"])
 app = Dash(__name__, external_stylesheets=[
            './css/style.css'])
 
+# Sidebar
 sidebar = html.Div([
     html.Div([
         html.Div([
-            html.Img(src="./assets/images/Analytics.png",
-                     className="sidebar__img")
-        ], style={"width": "30%", "display": "flex"}),
-        html.Div([html.Span("DASHBOARD -", style={"display": "block"},
-                 className="sidebar__topLine__span"),
-                  html.Span("Medidor de Cascão", style={"display": "block"},
-                 className="sidebar__topLine__span")])
+            html.Img(
+                src="./assets/images/Analytics.png",
+                    className="sidebar__img")],
+                 style={"width": "30%", "display": "flex"}),
+
+        html.Div([html.Span("DASHBOARD -",
+                            style={"display": "block"},
+                            className="sidebar__topLine__span"),
+                  html.Span("Medidor de Cascão",
+                            style={"display": "block"},
+                            className="sidebar__topLine__span")])
     ], className="sidebar__topLine"),
+
     html.Div(
-        [html.Span("Painel de controle", className="sidebar__title")], style={"text-align": "center", "margin": "3% 0"}),
+        [html.Span("Painel de controle",
+                   className="sidebar__title")],
+        style={"text-align": "center", "margin": "3% 0"}),
     html.Div(
-        [html.Img(src="./assets/images/lupa.png",
-                  className="sidebar__img")], className="sidebar__lupa"),
+        [html.Img(
+            src="./assets/images/lupa.png",
+                className="sidebar__img")],
+        className="sidebar__lupa"),
+
     html.Div(
-        [html.Button([html.Img(src="./assets/images/lapis.png",
-                               style={"width": "10%", "display": "inline-block", "padding-right ": "50%", "margin-right": "3%"}),
-                     html.Span("Iniciar Segmentação", className="sidebar__button__fonts")], className="sidebar__button")], style={"width": "74.74%"}),
+        [html.Button([html.Img(
+            src="./assets/images/lapis.png",
+            style={"width": "10%", "display": "inline-block", "padding-right ": "50%", "margin-right": "3%"}),
+            html.Span("Iniciar Segmentação",
+                      className="sidebar__button__fonts")],
+            className="sidebar__button")],
+        style={"width": "74.74%"}),
+
     html.Div(
-        [html.Img(src="./assets/images/oficinaC.png",
-                  className="sidebar__img")], className="sidebar__logo", style={"height": "5%"}),
+        [html.Img(
+            src="./assets/images/oficinaC.png",
+            className="sidebar__img")],
+        className="sidebar__logo",
+        style={"height": "5%"}),
+
     html.Div(
-        [html.Img(src="./assets/images/arcelor_logo.png",
-                  className="sidebar__img")], className="sidebar__logo", style={"height": "8%"}),
+        [html.Img(
+            src="./assets/images/arcelor_logo.png",
+            className="sidebar__img")],
+        className="sidebar__logo",
+        style={"height": "8%"}),
+
     html.Div(
-        [html.Img(src="./assets/images/ifes_logoC.png",
-                  className="sidebar__img")], className="sidebar__logo", style={"height": "15%"}),
+        [html.Img(
+            src="./assets/images/ifes_logoC.png",
+                className="sidebar__img")],
+        className="sidebar__logo",
+        style={"height": "15%"}),
 ], className="sidebar")
 
+# Primeiro Card
+card1 = html.Div([
+    html.Div(html.H2("Imagem Segmentada", style={"margin": "auto 0", "width": "100%"}), style={
+        "display": "flex", "height": "10%", "width": "100%"}),
+    # html.Img(src="./assets/images/youtube.png", style={"width": "50%", "max-height": "80%", "display": "block", "margin": "auto"})], className="card", style={"padding": "1.04%", "height": "100%"})
+    html.Div(
+        [html.Img(id="v1", style={"width": "100%", "max-height": "100%", "display": "block", "margin": "auto", })] +
+        [WebSocket(url="ws://127.0.0.1:5000/stream0", id="ws")], style={"height": "90%"})],
+    className="card", style={"padding": "1.04%", "height": "100%"})
 
-card1 = html.Div(
-    [#html.Span("Imagem Segmentada", style={"display":"block"}),
-    html.Img(src="./assets/images/youtube.png",style={"max-width":"100%","max-height":"100%","display":"block"})], className="card")
-# [html.Img(style={"width": "100%", "max-height": "100%"}, id=f"v{i}") for i in range(1)] +
-# [WebSocket(
-#     url=f"ws://127.0.0.1:5000/stream{i}", id=f"ws{i}") for i in range(1)], className="card")
-
-# Medidor
+# Segundo Card
 layout = dict(plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)')
-fig = html.Div([
-    dcc.Graph(figure=go.Figure(go.Indicator(
-        value=75,
-        mode="gauge+number+delta",
-        title={'text': "Diâmetro [mm]"},
-        delta={'reference': 40, "suffix": "mm"},
-        gauge={'axis': {'range': [40, 80]},
-               'steps': [
-            {'range': [40, 50], 'color': "greenyellow"},
-            {'range': [50, 60], 'color': "yellow"},
-            {'range': [60, 70], 'color': "orange"},
-            {'range': [70, 80], 'color': "red"}],
-            'threshold': {'line': {'color': "black", 'width': 8}, 'thickness': 0.75, 'value': 70},
-            'bar': {'color': "darkblue"},
-        }),
-        layout=layout),
-        style={"max-height": "100%", "width": "100%", "display": "block","overflow":"hidden"}),
-], style={"display": "flex", "width": "100%", "justify-content": "center"})
-
 card2 = html.Div([
     html.Div([
-        html.H2("Diâmetro Máximo [mm]:"),
-        daq.LEDDisplay(
-            id="operator-led",
-            value="1704",
-            color="#92e0d3",
-            backgroundColor="#1e2130",
-            size=20,
-        )], className="diam"), fig,
+        html.Div(html.H2("Diâmetro Máximo [mm]:", style={
+                 "margin": "auto 0", "width": "100%"}), style={"height": "auto", "display": "block"}),
+        html.Div(
+            [daq.LEDDisplay(
+                id="operator-led",
+                value="1704",
+                color="#92e0d3",
+                backgroundColor="#1e2130",
+                size=20)], style={"display": "block", "height": "15%", }),
+        html.Div([
+            dcc.Graph(figure=go.Figure(go.Indicator(
+                value=75,
+                mode="gauge+number+delta",
+                title={'text': "Diâmetro [mm]"},
+                delta={'reference': 40, "suffix": "mm"},
+                gauge={'axis': {'range': [40, 80]},
+                      'steps': [
+                    {'range': [40, 50], 'color': "greenyellow"},
+                    {'range': [50, 60], 'color': "yellow"},
+                    {'range': [60, 70], 'color': "orange"},
+                    {'range': [70, 80], 'color': "red"}],
+                    'threshold': {'line': {'color': "black", 'width': 8}, 'thickness': 0.75, 'value': 70},
+                    'bar': {'color': "darkblue"},
+                }),
+                layout=layout),
+                responsive=True,
+                style={"width": "100%", "overflow": "hidden", "height": "18.22vw", "display": "block"}),
+        ], style={"display": "flex", "width": "100%", "height": "70%", "margin": "auto 0"})
+    ]),
 
-], className="card")
+], className="card", style={"height": "100%"})
 
-# DataFrame
+# Leitura do dataframe para geração dos gráficos
 df = px.data.gapminder().query("country=='Canada'")
 fig = px.line(df, x="year", y="lifeExp",
               title='Life expectancy in Canada', color='country', symbol="country", template="lux")
@@ -98,28 +129,32 @@ fig.update_layout({
     "paper_bgcolor": "rgba(0, 0, 0, 0)",
 })
 
+# Terceiro Card
 card3 = html.Div([
     dcc.Graph(id='my-graph', figure=fig, responsive=True,
               style={"width": "100%", "height": "100%"})
 ], className="card2 sidebar__img")
 
+# Montagem do cards
 main = html.Div([
-    html.Div([card1, card2], style={
-             "display": "flex", "height": "60%", "width": "100%", "justify-content": "space-between", "padding": "2% 0 0", "box-sizing": "border-box"}),
-    html.Div(card3, style={"height": "40%",
-             "padding": "2% 0", "box-sizing": "border-box"})
+    html.Div(
+        [card1, card2],
+        className="main__c1c2"),
+    html.Div(
+        card3,
+        className="main__c3")
 ], className="main")
 
+# Inserção dos componentes no body
 app.layout = html.Div([
     html.Div([
         sidebar, main
     ], className="desktop"),
 ], className="container")
 
-
-# for i in range(1):
-#     app.clientside_callback("function(m){return m? m.data : '';}", Output(
-#         f"v{i}", "src"), Input(f"ws{i}", "message"))
+# Callback para exibição do video por servidor
+app.clientside_callback("function(m){return m? m.data : '';}", Output(
+    "v1", "src"), Input("ws", "message"))
 
 if __name__ == '__main__':
     port = "8000"
--- streaming.py
+++ streaming.py
@@ -2,14 +2,14 @@ import asyncio
 import base64
 import dash
 import cv2
-import dash_html_components as html
-import threading
+import numpy as np
 
 from dash.dependencies import Output, Input
 from quart import Quart, websocket
 from dash_extensions import WebSocket
+from ultralytics import YOLO
 
-
+model = YOLO('C:\\Users\\hewer\\OneDrive\\Área de Trabalho\\Medidor de cascão\\video_cascao\\best.pt') 
 class VideoCamera(object):
     def __init__(self, video_path):
         self.video = cv2.VideoCapture(video_path)
@@ -17,26 +17,46 @@ class VideoCamera(object):
     def __del__(self):
         self.video.release()
 
+    def calc_greater_distance(self,polygon):
+        greater_distance = 0
+        for point1 in polygon:
+            for point2 in polygon:
+                if point1[0][1] == point2[0][1] and abs(point1[0][0] - point2[0][0]) > greater_distance:
+                    greater_distance = abs(point1[0][0] - point2[0][0])
+                    p1=point1[0][0],point1[0][1]
+                    p2=point2[0][0],point2[0][1]
+        return p1,p2,greater_distance
+
     def get_frame(self):
 
         success, image = self.video.read()
 
-        # Recolor image to RGB
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
+        results = model(image,stream=True,verbose=False)
+        for result in results:
+            try:
+              mask=result[0].masks.xy
+              mask = np.array(mask, np.int32)
+              mask = mask.reshape((-1,1,2))
+              annotated_frame =cv2.polylines(result.plot(boxes=False,masks=True),[mask],True,(0,255,255))
 
-        # Recolor back to BGR
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+              # Cálculo dos pontos e distância entre os pontos
+              p1,p2,d = self.calc_greater_distance(mask)
+              position= p1[0],p1[1]-10
+              distancia = str(d) +" mm"
 
-        _, jpeg = cv2.imencode('.jpg', image)
+              annotated_frame = cv2.line(annotated_frame, p2,p1, (0, 255, 0) ,9)
+              annotated_frame = cv2.putText(annotated_frame, distancia, position ,cv2.FONT_HERSHEY_SIMPLEX,1,(0, 255, 0),3,cv2.LINE_AA )
+            except Exception as e:
+                print(e)
+                pass
+        #annotated_frame = results[0].plot()
+
+        _, jpeg = cv2.imencode('.jpg', annotated_frame)
         return jpeg.tobytes()
 
 
 # Setup small Quart server for streaming via websocket, one for each stream.
 server = Quart(__name__)
-n_streams = 1
-
 
 async def stream(camera, delay=None):
     while True:
@@ -48,28 +68,14 @@ async def stream(camera, delay=None):
 
 @server.websocket("/stream0")
 async def stream0():
-    camera = VideoCamera(0)
+    camera = VideoCamera("C:\\Users\\hewer\\OneDrive\\Área de Trabalho\\Medidor de cascão\\video_cascao\\CortadoDSC_0005.mp4")
     await stream(camera)
 
 
-@server.websocket("/stream1")
-async def stream1():
-    camera = VideoCamera('rtsp://192.168.100.43:8080/h264_ulaw.sdp')
-    await stream(camera)
-
-
-# Create small Dash application for UI.
-app = dash.Dash(__name__)
-app.layout = html.Div(
-    [html.Img(style={'width': '40%', 'padding': 10}, id=f"v{i}") for i in range(n_streams)] +
-    [WebSocket(
-        url=f"ws://127.0.0.1:5000/stream{i}", id=f"ws{i}") for i in range(n_streams)]
-)
-# Copy data from websockets to Img elements.
-for i in range(n_streams):
-    app.clientside_callback("function(m){return m? m.data : '';}", Output(
-        f"v{i}", "src"), Input(f"ws{i}", "message"))
+# @server.websocket("/stream1")
+# async def stream1():
+#     camera = VideoCamera('rtsp://192.168.100.43:8080/h264_ulaw.sdp')
+#     await stream(camera)
 
 if __name__ == '__main__':
-    threading.Thread(target=app.run_server).start()
     server.run()
